98e29ead6b307e79beb2c517ee81205fcb92a7c0
Mohammed Taha <mohammedtaha.hashmi@gmail.com>
2026-05-18 16:01:28 +0530

    updated keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select All", noremap = true, silent = true })
+
+-- Copy (ctrl+c)
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard", noremap = true, silent = true })
+
+-- Paste (ctrl+v)
+vim.keymap.set("n", "<C-v>", '"+p', { desc = "Paste from clipboard", noremap = true, silent = true })
+vim.keymap.set("i", "<C-v>", "<C-r>+", { desc = "Paste from clipboard", noremap = true, silent = true })
