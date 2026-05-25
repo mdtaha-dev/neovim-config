039febc22e643b6f2b7510f6d2ddc062a86d670f
Mohammed Taha <mohammedtaha.hashmi@gmail.com>
2026-05-25 20:58:49 +0530

    autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    local col = vim.fn.col(".")
+    if col > 1 then
+      vim.fn.cursor(vim.fn.line("."), col + 1)
+    end
+  end,
+})
+
+vim.keymap.set("i", "<C-BS>", "<C-w>")
